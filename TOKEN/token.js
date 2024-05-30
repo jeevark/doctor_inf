76cad71d18bde6d26472148bdd6ac0526c782fb9
jeevark <rjeeva1706@gmail.com>
2024-05-30 03:22:37 -0700

    token

--- TOKEN/token.js
+++ TOKEN/token.js
@@ -1,18 +1,18 @@
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-// const authenticateToken = (req,res,next)=>{
-//     const authHeader = req.headers.authorization;
-//     const token = authHeader && authHeader.split(' ')[1];
-//     console.log(token);
-//     if(!token){
-//       return res.Status(403).send(err);
-//     }
-//     jwt.verify(token,process.env.ACCESS_TOKEN,(err,user)=>{
-//       if(err){
-//         res.Status(403).send("verify Error.........")
-//       }
-//       req.user =user; 
-//       next();
-//     })
-//   }  
-//   exports.module = {authenticateToken };
+const authenticateToken = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
+    console.log(token);
+    if(!token){
+      return res.Status(403).send(err);
+    }
+    jwt.verify(token,process.env.ACCESS_TOKEN,(err,user)=>{
+      if(err){
+        res.Status(403).send("verify Error.........")
+      }
+      req.user =user; 
+      next();
+    })
+  }  
+  exports.module = {authenticateToken };
